7b7eb12ddbb7e87fcfc185c4cb05c384e1841392
Lecodeurfou <32478789+Lecodeurfou@users.noreply.github.com>
2019-02-18 17:56:44 +0100

    add try event on hover
    essai d'un event sur le bouton hover

---
+++ Assets/ActiveDrawingOn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveDrawingOn : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    public void SetTest()
+    {
+        GameObject obj = GameObject.Find("PinchDrawing")as GameObject;
+        if (obj.SetActive(true)) {
+            obj.SetActive(false);
+        }
+
+        if (obj.SetActive(false))
+        {
+            obj.SetActive(true);
+        }
+
+        //PinchDrawing.SetActive(false);
+    }
+
+}
---
+++ Assets/ScripHandmade/ActiverpinchDraw.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activerdessin : MonoBehaviour
+{
+
+    public GameObject PinchDrawing;
+    // Start is called before the first frame update
+    void SetDrawingOn()
+    {
+        PinchDrawing.SetActive(false);
+    }
+
+
+}
